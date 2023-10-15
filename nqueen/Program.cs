7caef57288ee906c201320d0d3bab1bd61a634be
Helen <4798413-qscwaz77499@users.noreply.gitlab.com>
2023-10-15 08:04:46 +0800

    update

--- nqueen/Program.cs
+++ nqueen/Program.cs
@@ -5,42 +5,48 @@ namespace ConsoleApplication1
     internal class Program
     {
         private static int _count = 0;
+
         public static void Main(string[] args)
         {
-            const int number = 8;
-        
-            var array = new int[number, number];
-            Backtracking(number,array);
+            const int number = 4;
 
+            var array = new int[number, number];
+            Backtracking(number, array);
         }
-        
-        
-        private static void Backtracking(int number,int[,] checkerboard){
-            if(number > 0){
-                for(var i = 0; i < checkerboard.GetLength(0) ; i++)
+
+
+        private static void Backtracking(int number, int[,] checkerboard)
+        {
+            var length = checkerboard.GetLength(0);
+
+            if (number > 0)
+            {
+                for (var i = 0; i < length; i++)
                 {
                     if (ForwardChecking(i, number - 1, checkerboard))
                     {
-                        checkerboard[i,number-1] = 1;
-                        Backtracking(number-1,checkerboard);
-                        checkerboard[i,number-1] = 0;
+                        
+                        checkerboard[i, number - 1] = 1;
+                        Backtracking(number - 1, checkerboard);
+                        checkerboard[i, number - 1] = 0;
                     }
                 }
-            
-            }else
+            }
+            else
             {
                 _count++;
-                Console.WriteLine("第:" + _count+"個");
-                for(var i = 0; i < checkerboard.GetLength(0) ;i++)
+                Console.WriteLine("//Solution " + _count );
+                for (var i = 0; i < length; i++)
                 {
-                    for(var j=0;j < checkerboard.GetLength(1);j++)
+                    for (var j = 0; j < length; j++)
                     {
                         Console.Write(checkerboard[i, j] == 0 ? "." : "Q");
                     }
-                    Console.WriteLine ();
+
+                    Console.WriteLine();
                 }
-                Console.WriteLine ();
-            
+
+                Console.WriteLine();
             }
         }
 
@@ -48,25 +54,25 @@ namespace ConsoleApplication1
         {
             var length = checkerboard.GetLength(0);
             int s, t;
-            for(s=i,t=0; t<length; t++)
-                if(checkerboard[s,t]==1 && t!=j)
-                    return false;//判斷行
-            for(t=j,s=0; s<length; s++)
-                if(checkerboard[s,t]==1 && s!=i)
-                    return false;//判斷列
-            for(s=i-1,t=j-1; s>=0&&t>=0; s--,t--)
-                if(checkerboard[s,t]==1)
-                    return false;//判斷左上方
-            for(s=i+1,t=j+1; s<length&&t<length;s++,t++)
-                if(checkerboard[s,t]==1)
-                    return false;//判斷右下方
-            for(s=i-1,t=j+1; s>=0&&t<length; s--,t++)
-                if(checkerboard[s,t]==1)
-                    return false;//判斷右上方
-            for(s=i+1,t=j-1; s<length&&t>=0; s++,t--)
-                if(checkerboard[s,t]==1)
-                    return false;//判斷左下方
-            return true;//否則返回
+            for (s = i, t = 0; t < length; t++)
+                if (checkerboard[s, t] == 1 && t != j)
+                    return false; //判斷行
+            for (t = j, s = 0; s < length; s++)
+                if (checkerboard[s, t] == 1 && s != i)
+                    return false; //判斷列
+            for (s = i - 1, t = j - 1; s >= 0 && t >= 0; s--, t--)
+                if (checkerboard[s, t] == 1)
+                    return false; //判斷左上方
+            for (s = i + 1, t = j + 1; s < length && t < length; s++, t++)
+                if (checkerboard[s, t] == 1)
+                    return false; //判斷右下方
+            for (s = i - 1, t = j + 1; s >= 0 && t < length; s--, t++)
+                if (checkerboard[s, t] == 1)
+                    return false; //判斷右上方
+            for (s = i + 1, t = j - 1; s < length && t >= 0; s++, t--)
+                if (checkerboard[s, t] == 1)
+                    return false; //判斷左下方
+            return true; //否則返回
         }
     }
 }
